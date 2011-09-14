a2eb68cfa53536f8d381a6ee0af3fc657f2cab08
Johan Kool <johankool@gmail.com>
2011-09-14 16:04:54 +0800

    Fix comment style

--- Classes/EFRichTextView.h
+++ Classes/EFRichTextView.h
@@ -12,30 +12,42 @@
 
 @interface EFRichTextView : UIScrollView
 
-/* The text to be rendered, should be either an NSString or an
- * NSAttributedString. Defaults to nil. */
+/* 
+ * The text to be rendered, should be either an NSString or an
+ * NSAttributedString. Defaults to nil. 
+ */
 @property(copy) id string;
 
-/* The font to use. Defaults to the Helvetica font. Only
- * used when the `string' property is not an NSAttributedString. */
+/* 
+ * The font to use. Defaults to the Helvetica font. Only
+ * used when the `string' property is not an NSAttributedString. 
+ */
 @property(retain) UIFont *font;
 
-/* The color object used to draw the text. Defaults to opaque white.
- * Only used when the `string' property is not an NSAttributedString. */
+/* 
+ * The color object used to draw the text. Defaults to opaque white.
+ * Only used when the `string' property is not an NSAttributedString. 
+ */
 @property(retain) UIColor *foregroundColor;
 
-/* When true the string is wrapped to fit within the view bounds.
- * Defaults to false.*/
+/* 
+ * When true the string is wrapped to fit within the view bounds.
+ * Defaults to false.
+ */
 @property(getter=isWrapped) BOOL wrapped;
 
-/* Describes how the string is truncated to fit within the layer
+/* 
+ * Describes how the string is truncated to fit within the layer
  * bounds. The possible options are `none', `start', `middle' and
- * `end'. Defaults to `none'. */
+ * `end'. Defaults to `none'. 
+ */
 @property(copy) NSString *truncationMode;
 
-/* Describes how individual lines of text are aligned within the layer
+/* 
+ * Describes how individual lines of text are aligned within the layer
  * bounds. The possible options are `natural', `left', `right',
- * `center' and `justified'. Defaults to `natural'. */
+ * `center' and `justified'. Defaults to `natural'. 
+ */
 @property(copy) NSString *alignmentMode;
 
 - (CGSize)frameSize;
