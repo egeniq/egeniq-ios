99a8e30d0fe3bdcb5c2119a4a6096c3cb634a579
Johan Kool <johan@koolistov.net>
2012-04-18 14:23:03 +0800

    Setup Kiwi

---
+++ EgeniqTests/EFDayTests.m
@@ -0,0 +1,21 @@
+//
+//  EFDayTests.m
+//  Egeniq
+//
+//  Created by Johan Kool on 18/4/2012.
+//  Copyright (c) 2012 Egeniq. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(EFDayTests)
+
+describe(@"Math", ^{
+    it(@"is pretty cool", ^{
+        NSUInteger a = 16;
+        NSUInteger b = 26;
+        [[theValue(a + b) should] equal:theValue(42)];
+    });
+});
+
+SPEC_END
