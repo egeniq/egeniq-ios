c3a2a37c3b90046f22d9b622b9a538761d8b3680
Thijs Damen <thijs@egeniq.com>
2013-11-06 11:07:04 +0100

    Modified border settin

--- Classes/EFSettingsButton.m
+++ Classes/EFSettingsButton.m
@@ -24,15 +24,10 @@
         self.backgroundColor = [UIColor whiteColor];
         
         self.topBorder = [CALayer layer];
-        self.topBorder.frame = CGRectMake(0.0f, 0.0f, self.frame.size.width, 1.0f);
-        [self.layer addSublayer:self.topBorder];
-        
         self.bottomBorder = [CALayer layer];
-        self.bottomBorder.frame = CGRectMake(0.0f, self.frame.size.height - 1.0f, self.frame.size.width, 1.0f);
-        [self.layer addSublayer:self.bottomBorder];
-        
-        self.borderVisible = YES;
         self.borderColor = [UIColor colorWithRed:224.0f/255.0f green:224.0f/255.0f blue:224.0f/255.0f alpha:1.0f].CGColor;
+        
+        [self setBorderVisible:YES];
     }
     return self;
 }
@@ -41,34 +36,27 @@
     [super layoutSubviews];
     
     if (self.isBorderVisible) {
-        if (self.topBorder) {
-            self.topBorder.backgroundColor = self.borderColor;
-            self.topBorder.frame = CGRectMake(0.0f, 0.0f, self.frame.size.width, 1.0f);
-        }
-        
-        if (self.bottomBorder) {
-            self.bottomBorder.backgroundColor = self.borderColor;
-            self.bottomBorder.frame =CGRectMake(0.0f, self.frame.size.height - 1.0f, self.frame.size.width, 1.0f);
-        }
-    } else {
-        if (self.topBorder) {
-            [self.topBorder removeFromSuperlayer];
-        }
-        
-        if (self.bottomBorder) {
-            [self.bottomBorder removeFromSuperlayer];
-        }
+        self.topBorder.frame = CGRectMake(0.0f, 0.0f, self.frame.size.width, 1.0f);
+        self.bottomBorder.frame =CGRectMake(0.0f, self.frame.size.height - 1.0f, self.frame.size.width, 1.0f);
     }
-
 }
 
 - (void)setBorderVisible:(BOOL)borderVisible {
     _borderVisible = borderVisible;
+    if (borderVisible) {
+        [self.layer addSublayer:self.topBorder];
+        [self.layer addSublayer:self.bottomBorder];
+    } else {
+        [self.topBorder removeFromSuperlayer];
+        [self.bottomBorder removeFromSuperlayer];
+    }
     [self setNeedsDisplay];
 }
 
 - (void)setBorderColor:(CGColorRef)borderColor {
     _borderColor = borderColor;
+    self.topBorder.backgroundColor = self.borderColor;
+    self.bottomBorder.backgroundColor = self.borderColor;
     [self setNeedsDisplay];
 }
 
